cc4b2c2892523ec43c627c539f634f6846cbee57
vitor <vitorvtlopes@gmail.com>
2026-05-30 16:08:53 -0300

    Terminando implementação das classes Tutor nas camadas de controlador e serviço

--- src/main/java/br/com/veterinaryclinicapi/application/service/TutorService.java
+++ src/main/java/br/com/veterinaryclinicapi/application/service/TutorService.java
@@ -4,7 +4,9 @@ import br.com.veterinaryclinicapi.application.dto.request.TutorRequest;
 import br.com.veterinaryclinicapi.application.dto.response.TutorResponse;
 import br.com.veterinaryclinicapi.domain.model.Tutor;
 import br.com.veterinaryclinicapi.domain.repository.TutorRespository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,23 +22,21 @@ public class TutorService {
 
     public TutorResponse save(TutorRequest tutor) {
         if (this.verifyTutorExists(tutor.getCpf())) {
-            System.out.println("cpf já está sendo utilizado");
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "CPF already in use");
         }
 
-        repository.save(this.toDomain(tutor));
-        return toResponse(tutor);
+        Tutor savedTutor = repository.save(this.toDomain(tutor));
+        return toResponse(savedTutor);
     }
 
-    public void delete(TutorRequest tutor) {
-        if (this.verifyTutorExists(tutor.getCpf())) {
-            repository.delete(toDomain(tutor));
-        }
-        System.out.println("Tutor does not exist");
+    public void delete(Long id) {
+        Tutor tutor = this.findById(id);
+        repository.delete(tutor);
     }
 
     public List<TutorRequest> findAll() {
         List<TutorRequest> tutors = new ArrayList<>();
-        for (Tutor tutorRequest : this.repository.findAll()){
+        for (Tutor tutorRequest : this.repository.findAll()) {
             tutors.add(this.toRequest(tutorRequest));
         }
         return tutors;
@@ -45,10 +45,36 @@ public class TutorService {
     public Tutor findById(Long id) {
         Tutor tutor = this.repository.findById(id);
         if (tutor == null) {
-            System.out.println("Tutor with id " + id + " does not exist");
-            return null;
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Tutor with id " + id + " does not exist");
         }
         return tutor;
+    }
+
+    public TutorRequest findByIdAsRequest(Long id) {
+        return toRequest(findById(id));
+    }
+
+    public TutorResponse patch(Long id, TutorRequest tutorRequest) {
+        Tutor tutor = this.findById(id);
+
+        if (tutorRequest.getName() != null) {
+            tutor.setName(tutorRequest.getName());
+        }
+        if (tutorRequest.getEmail() != null) {
+            tutor.setEmail(tutorRequest.getEmail());
+        }
+        if (tutorRequest.getCpf() != null) {
+            tutor.setCpf(tutorRequest.getCpf());
+        }
+        if (tutorRequest.getPhone() != null) {
+            tutor.setPhone(tutorRequest.getPhone());
+        }
+        if (tutorRequest.getAddress() != null) {
+            tutor.setAddress(tutorRequest.getAddress());
+        }
+
+        Tutor savedTutor = repository.save(tutor);
+        return toResponse(savedTutor);
     }
 
     private boolean verifyTutorExists(String cpf) {
@@ -65,10 +91,11 @@ public class TutorService {
                 tutorRequest.getPhone(), tutorRequest.getAddress());
     }
 
-    private TutorResponse toResponse(TutorRequest tutorRequest) {
-        return new TutorResponse(tutorRequest.getName(), tutorRequest.getEmail(), tutorRequest.getCpf(),
-                tutorRequest.getPhone(), tutorRequest.getAddress());
+    private TutorResponse toResponse(Tutor tutor) {
+        return new TutorResponse(tutor.getName(), tutor.getEmail(), tutor.getCpf(), tutor.getPhone(),
+                tutor.getAddress());
     }
+
     private TutorRequest toRequest(Tutor tutor) {
         return new TutorRequest(tutor.getId(), tutor.getName(), tutor.getEmail(), tutor.getCpf(), tutor.getPhone(),
                 tutor.getAddress());
--- src/main/java/br/com/veterinaryclinicapi/presentation/TutorController.java
+++ src/main/java/br/com/veterinaryclinicapi/presentation/TutorController.java
@@ -3,34 +3,96 @@ package br.com.veterinaryclinicapi.presentation;
 import br.com.veterinaryclinicapi.application.dto.request.TutorRequest;
 import br.com.veterinaryclinicapi.application.dto.response.TutorResponse;
 import br.com.veterinaryclinicapi.application.service.TutorService;
-import br.com.veterinaryclinicapi.domain.model.Tutor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * Controller REST responsavel por expor as operacoes de cadastro e consulta de tutores.
+ * <p>
+ * A classe centraliza os endpoints HTTP relacionados ao recurso {@code /tutors} e delega
+ * as regras de negocio para {@link TutorService}.
+ */
 @RequestMapping("/tutors")
 @RestController
 public class TutorController {
 
     private final TutorService tutorService;
 
+    /**
+     * Cria uma instancia do controller com o service necessario para manipular tutores.
+     *
+     * @param tutorService service responsavel pelas regras de negocio de tutores
+     */
     public TutorController(TutorService tutorService) {
         this.tutorService = tutorService;
     }
 
-
-    // Exemplo e estudar mais sobre requisições POST
+    /**
+     * Cadastra um novo tutor.
+     *
+     * @param tutorRequest dados do tutor que sera cadastrado
+     * @return dados do tutor cadastrado
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public TutorResponse addTutor(@RequestBody TutorRequest tutorRequest) {
         return this.tutorService.save(tutorRequest);
     }
 
-    // Exemplo e estudar mais sobre requisições GET
+    /**
+     * Lista todos os tutores cadastrados.
+     *
+     * @return lista de tutores cadastrados
+     */
     @GetMapping
-    public List<TutorRequest> getAllTutors(){
+    public List<TutorRequest> getAllTutors() {
         return this.tutorService.findAll();
+    }
+
+    /**
+     * Busca um tutor pelo identificador.
+     *
+     * @param id identificador do tutor
+     * @return dados do tutor encontrado
+     */
+    @GetMapping("/{id}")
+    public TutorRequest findById(@PathVariable Long id) {
+        return this.tutorService.findByIdAsRequest(id);
+    }
+
+    /**
+     * Atualiza parcialmente os dados de um tutor.
+     * <p>
+     * Apenas os campos enviados no corpo da requisicao sao alterados.
+     *
+     * @param id identificador do tutor que sera atualizado
+     * @param tutorRequest campos do tutor que devem ser alterados
+     * @return dados do tutor atualizado
+     */
+    @PatchMapping("/{id}")
+    public TutorResponse patchTutor(@PathVariable Long id, @RequestBody TutorRequest tutorRequest) {
+        return this.tutorService.patch(id, tutorRequest);
+    }
+
+    /**
+     * Remove um tutor pelo identificador.
+     *
+     * @param id identificador do tutor que sera removido
+     */
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteTutor(@PathVariable Long id) {
+        this.tutorService.delete(id);
     }
 
 }
