b699d43fa63b275082fbc8c753a13ad699cbc5c7
vitor <vitorvtlopes@gmail.com>
2026-06-05 02:19:56 -0300

    Criando ConsultationService

--- src/main/java/br/com/veterinaryclinicapi/application/service/ConsultationService.java
+++ src/main/java/br/com/veterinaryclinicapi/application/service/ConsultationService.java
@@ -1,15 +1,54 @@
 package br.com.veterinaryclinicapi.application.service;
 
+import br.com.veterinaryclinicapi.application.dto.request.ConsultationRequest;
+import br.com.veterinaryclinicapi.application.dto.response.ConsultationResponse;
+import br.com.veterinaryclinicapi.application.mapper.ConsultationDtoMapper;
+import br.com.veterinaryclinicapi.domain.model.Consultation;
 import br.com.veterinaryclinicapi.domain.repository.ConsultationRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ConsultationService {
 
-    private ConsultationRepository repository;
+    private final ConsultationRepository repository;
 
-    public ConsultationService(ConsultationRepository repository) {
+    private final ConsultationDtoMapper mapper;
+
+    public ConsultationService(ConsultationRepository repository, ConsultationDtoMapper mapper) {
         this.repository = repository;
+        this.mapper = mapper;
     }
 
+    public ConsultationResponse save(ConsultationRequest consultationRequest) {
+        Consultation saveConsultation = this.repository.save(mapper.toDomain(consultationRequest));
+        return mapper.toResponse(saveConsultation);
+    }
+
+    public ConsultationResponse findById(Long id) {
+        Consultation consultation = this.repository.findById(id);
+
+        if (consultation == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Consultation Not Found");
+        }
+
+        return mapper.toResponse(consultation);
+    }
+
+    public List<ConsultationRequest> findAll() {
+        List<ConsultationRequest> consultations = new ArrayList<>();
+        for (Consultation consultation : this.repository.findAll()) {
+            consultations.add(mapper.toRequest(consultation));
+        }
+        return consultations;
+    }
+
+    public void remove(Long id) {
+        Consultation consultation = this.repository.findById(id);
+        this.repository.delete(consultation);
+    }
 }
